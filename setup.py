8d0f3fe015f8b9d500024577356a8aebabed8b5d
ewm9759 <ewm9759@uncw.edu>
2015-01-12 20:14:13 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='pytunes',
+      version='0.2.2',
+      description = 'A lightweight command-line music player',
+      author = 'Eli Mullis',
+      scripts=['pytunes.py', 'TrackQueue.py'])
