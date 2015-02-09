a0de04ca2eb6a63f8455f1c671cde96e3c0fffeb
Eli Mullis <elimullis91@gmail.com>
2015-02-08 18:19:04 -0800

    Added prompt for failed Last.FM login case

--- pytunes.py
+++ pytunes.py
@@ -42,13 +42,14 @@ conf.read("pytunes.conf")
    files (afplay or aplay).'''
 if not conf.get('sys', 'player'):
     os_fam = platform.system()
+    print "OS Detected: %s"
     if os_fam == "Darwin":
         sys_player = "afplay"
     elif os_fam == "Linux":
         sys_player = "mpg123"
 else: 
     sys_player = conf.get('sys', 'player')
-
+print sys_player
 
 def get_metadata(filepath):
     # todo: read config file for track metadata
@@ -84,33 +85,37 @@ def main():
         # Import scrobbling dependencies
         import pylast
 
-        try:
-            API_KEY = conf.get('lastfm', 'API_KEY')
-            API_SECRET = conf.get('lastfm', 'API_SECRET')
+        API_KEY = conf.get('lastfm', 'API_KEY')
+        API_SECRET = conf.get('lastfm', 'API_SECRET')
          
-            username = conf.get('lastfm', 'user')
-            password_hash = conf.get('lastfm', 'pass')
-            
-            if not username or not password_hash:
-                username = raw_input("Last.FM username: ")
-                password_hash = pylast.md5(getpass.getpass("Last.FM password: "))
+        username = conf.get('lastfm', 'user')
+        password_hash = conf.get('lastfm', 'pass')
 
-            print "Logging user %s into Last.FM..." % username
+        if not username or not password_hash:
+            username = raw_input("Last.FM username: ")
+            password_hash = pylast.md5(getpass.getpass("Last.FM password: "))
 
+        print "Logging user %s into Last.FM..." % username
+
+        try:
             NET = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET,
                 username = username, password_hash = password_hash)
             SESSION_KEY = pylast.SessionKeyGenerator(NET).get_session_key(
                 username, password_hash)
             NET.session_key = SESSION_KEY
         
-        #todo: Improve exception handling
         except:
             print "An exception occurred."
-            sys.exit()
+            cont = ''
+            while cont.lower() != 'y':
+                cont = raw_input("Continue without scrobbling (y/n)? ")
+                if cont.lower() == 'n':
+                    sys.exit()
 
     while not tqueue.empty():
         system('clear')
         track_path = tqueue.get()
+        print track_path
         command = "%s \"%s\"" % (sys_player, track_path)
         artist, title, album = get_metadata(track_path)
         print "Now playing: \n%s\n%s\n%s" % (artist, title, album)
